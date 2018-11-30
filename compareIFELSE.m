62a3164584d5c1f9d49b0cb1b5630eda309d2440
Derian <38805233+muhammadderian@users.noreply.github.com>
2018-11-30 23:50:55 +0700

    Add files via upload

---
+++ compareIFELSE.m
@@ -0,0 +1,13 @@
+%perbandingan angka terbesar dari 2 buah angka dengan manggunakan
+%algoritma percabangan dimana kedua angka tersebut inputan oleh user
+
+A = input('silahkan input bilangan A : ');
+B = input('silahkan input bilangan B : ');
+
+if (A>B)
+    disp('Bilangan A lebih besar dari pada B');
+elseif(A<B)
+    disp('Bilangan B lebih besar dari pada A');
+else
+    disp('Bilangan A sama dengan B');
+end
