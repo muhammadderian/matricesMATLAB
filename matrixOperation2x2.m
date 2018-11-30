8b759c633e533a07457f6c02259ec33dd8c1cec6
Derian <38805233+muhammadderian@users.noreply.github.com>
2018-11-30 23:42:38 +0700

    Add files via upload

---
+++ matrixOperation2x2.m
@@ -0,0 +1,31 @@
+% Matrix 2x2 abstract value
+A = [ 10 9;8 7 ]
+B = [ 6 5;4 3 ]
+
+% Addition
+disp("Matrices A + B");
+A+B
+% Subtract
+disp("Matrices A - B");
+A-B
+% Devide
+disp("Matrices A / B");
+A/B
+% Power(square)
+r = input('Input the power = ');
+disp('Matrices A');
+A^r
+disp('Matrices B');
+B^r
+% Multiply
+disp("Matrices A x B");
+A*B
+% Multiply Element
+disp("Matrices A x B Element");
+A.*B
+% Multiply Scalar
+Number = input('Input Nember for Multiply to Matrices = ');
+disp('Matrices A');
+A*Number
+disp('Matrices B');
+B*Number
